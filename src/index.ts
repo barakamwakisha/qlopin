267bf9146dddcb261d53800b39436b47ed5781fb
barakamwakisha <barakamwakisha@gmail.com>
2023-09-12 07:50:46 +0300

    refactor: add typedefs loader option

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,29 @@
 import { GraphQLSchema, buildSchema, printSchema } from "graphql";
 
 import { generateListOptions } from './generate-list-options';
-import { generateErrorCodeEnum } from './generate-error-code-enum';
 import { getCommonTypesSchema } from "./get-common-types-schema";
+import { generateErrorCodeEnum } from './generate-error-code-enum';
 
 export type QlopinOptions = {
-    /**
-     * The GraphQL schema to generate types for.
-     * 
-     * This shouldd be string of SDL.
-     */
-    schema: string;
+    typeDefs: string;
+} | {
+    typeDefsLoader: () => Promise<string>;
 }
 
-export function buildQlopinSchema(options: QlopinOptions): GraphQLSchema {
-    const { schema } = options;
-    let qlopinSchema = buildSchema(`
-        ${printSchema(getCommonTypesSchema())}
-        ${schema}
-    `);
+export async function buildQlopinSchema(options: QlopinOptions): Promise<GraphQLSchema> {
+    let qlopinSchema: GraphQLSchema;
+
+    if ('typeDefs' in options) {
+        qlopinSchema = buildSchema(`
+            ${printSchema(getCommonTypesSchema())}
+            ${options.typeDefs}
+        `);
+    } else {
+        qlopinSchema = buildSchema(`
+            ${printSchema(getCommonTypesSchema())}
+            ${await options.typeDefsLoader()}
+        `);
+    }
 
     qlopinSchema = generateListOptions(qlopinSchema);
     qlopinSchema = generateErrorCodeEnum(qlopinSchema);
