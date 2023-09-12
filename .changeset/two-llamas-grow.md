0f377e268ed878383cd663b1c2c6aaadc3e323fb
barakamwakisha <barakamwakisha@gmail.com>
2023-09-12 15:19:47 +0300

    refactor: accept typedefs or typedefs glob paths as api options

---
+++ .changeset/two-llamas-grow.md
@@ -0,0 +1,5 @@
+---
+"@barakamwakisha/qlopin": minor
+---
+
+Refactory api to accept typedefs string or glob path to files with type definitions
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,43 @@
-import { GraphQLSchema, buildSchema, printSchema } from "graphql";
+import { mergeTypeDefs } from "@graphql-tools/merge";
+import { loadFilesSync } from "@graphql-tools/load-files";
+import { GraphQLSchema, buildSchema, printSchema, print } from "graphql";
 
 import { generateListOptions } from './generate-list-options';
 import { getCommonTypesSchema } from "./get-common-types-schema";
 import { generateErrorCodeEnum } from './generate-error-code-enum';
 
 export type QlopinOptions = {
+    /*
+    * Glob pattern for type definitions files.
+    */
+    typeDefsPath: string;
+} | {
+    /*
+    * SDL string for type definitions.
+    */
     typeDefs: string;
 }
 
+/**
+ * Builds a modified schema from type definitions.
+ * 
+ * @param options Options for building the schema.
+ * @returns The Qlopin GraphQL schema.
+ * 
+ **/
 export function buildQlopinSchema(options: QlopinOptions): GraphQLSchema {
+    let typeDefs: string;
+
+    if ('typeDefs' in options) {
+        typeDefs = options.typeDefs;
+    } else {
+        const typeDefsObjects = loadFilesSync(options.typeDefsPath);
+        typeDefs = print(mergeTypeDefs(typeDefsObjects));
+    }
+
     let qlopinSchema = buildSchema(`
         ${printSchema(getCommonTypesSchema())}
-        ${options.typeDefs}
+        ${typeDefs}
     `);
 
     qlopinSchema = generateListOptions(qlopinSchema);
